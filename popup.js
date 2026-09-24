d2b0820af58ab5fafebe02e0b0018696e089679b
Matheus Mol <matheusmol@hotmail.com>
2026-09-24 01:05:22 -0300

    Redesign popup with light/dark theme

--- popup.js
+++ popup.js
@@ -1,12 +1,20 @@
 const defaults = { autoClick: true, skipStart: false, skipSeconds: 300 };
 
+function syncSecondsRow() {
+  secondsRow.classList.toggle("disabled", !skipStart.checked);
+}
+
 chrome.storage.sync.get(defaults, (s) => {
   autoClick.checked = s.autoClick;
   skipStart.checked = s.skipStart;
   skipSeconds.value = s.skipSeconds;
+  syncSecondsRow();
 });
 
 autoClick.onchange = () => chrome.storage.sync.set({ autoClick: autoClick.checked });
-skipStart.onchange = () => chrome.storage.sync.set({ skipStart: skipStart.checked });
+skipStart.onchange = () => {
+  chrome.storage.sync.set({ skipStart: skipStart.checked });
+  syncSecondsRow();
+};
 skipSeconds.onchange = () =>
   chrome.storage.sync.set({ skipSeconds: Number(skipSeconds.value) || 0 });
